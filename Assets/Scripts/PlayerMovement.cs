580dd2e845f9fc69e39e4d0e8d91309bc4db7754
Cristian-zucconi <cri.zucconi@stud.uniroma3.it>
2024-04-16 19:20:16 +0200

    2videoCri

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horizontalMultiplayer = 2;
+
+    private void FixedUpdate(){
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplayer;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
